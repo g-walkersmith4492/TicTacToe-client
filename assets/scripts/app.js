66c4f366fb2f15c1058ef3e8b29fd73eb86a6759
g-walkersmith4492 <g.walkersmith4492@gmail.com>
2019-01-04 14:24:25 -0500

    Complete Functioning of createboard
    Add code to make sure box is locked and cannot enter multiple values into a single box.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,137 +5,138 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
-const playerMoves = []
+const playerMoves = [null, null, null, null, null, null, null, null, null]
 
+let lastMove = null
 const playerOne = 'X'
 const playerTwo = 'O'
 
 $(() => {
-
-    $('#box1').on('click', function () {
-      if ($('#box1').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
-    $('#box2').on('click', function () {
-      if ($('#box2').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
-    $('#box3').on('click', function () {
-      if ($('#box3').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
-    $('#box4').on('click', function () {
-      if ($('#box4').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
-    $('#box5').on('click', function () {
-      if ($('#box5').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
-    $('#box6').on('click', function () {
-      if ($('#box6').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
-    $('#box7').on('click', function () {
-      if ($('#box7').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
-    $('#box8').on('click', function () {
-      if ($('#box8').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
-    $('#box9').on('click', function () {
-      if ($('#box9').html('')) {
-        event.preventDefault()
-        if (playerMoves.length % 2 === 0) {
-          (event.target).append(playerOne)
-          playerMoves.push(playerOne)
-          console.log(playerMoves)
-        } else {
-          (event.target).append(playerTwo)
-          playerMoves.push(playerTwo)
-          console.log(playerMoves)
-        }
-      }
-    })
+  $('#box1').on('click', function () {
+    if ($('#box1').html() === '') {
+      (event.target).append(playerOne)
+      playerMoves[0] = playerOne
+      lastMove = playerOne
+    }
   })
+  $('#box2').on('click', function () {
+    if ($('#box2').html() === '') {
+      if (lastMove === playerTwo) {
+        (event.target).append(playerOne)
+        playerMoves[1] = playerOne
+        console.log(playerMoves)
+        lastMove = playerOne
+      } else {
+        (event.target).append(playerTwo)
+        playerMoves[1] = playerTwo
+        console.log(playerMoves)
+        lastMove = playerTwo
+      }
+    }
+  })
+  $('#box3').on('click', function () {
+    if ($('#box3').html() === '') {
+      if (lastMove === playerTwo) {
+        (event.target).append(playerOne)
+        playerMoves[2] = playerOne
+        console.log(playerMoves)
+        lastMove = playerOne
+      } else {
+        (event.target).append(playerTwo)
+        playerMoves[4] = playerTwo
+        console.log(playerMoves)
+        lastMove = playerTwo
+      }
+    }
+  })
+  $('#box4').on('click', function () {
+    if ($('#box4').html() === '') {
+      if (lastMove === playerTwo) {
+        (event.target).append(playerOne)
+        playerMoves[3] = playerOne
+        console.log(playerMoves)
+        lastMove = playerOne
+      } else {
+        (event.target).append(playerTwo)
+        playerMoves[3] = playerTwo
+        console.log(playerMoves)
+        lastMove = playerTwo
+      }
+    }
+  })
+  $('#box5').on('click', function () {
+    if ($('#box5').html() === '') {
+      if (lastMove === playerTwo) {
+        (event.target).append(playerOne)
+        playerMoves[4] = playerOne
+        console.log(playerMoves)
+        lastMove = playerOne
+      } else {
+        (event.target).append(playerTwo)
+        playerMoves[4] = playerTwo
+        console.log(playerMoves)
+        lastMove = playerTwo
+      }
+    }
+  })
+  $('#box6').on('click', function () {
+    if ($('#box6').html() === '') {
+      if (lastMove === playerTwo) {
+        (event.target).append(playerOne)
+        playerMoves[5] = playerOne
+        console.log(playerMoves)
+        lastMove = playerOne
+      } else {
+        (event.target).append(playerTwo)
+        playerMoves[5] = playerTwo
+        console.log(playerMoves)
+        lastMove = playerTwo
+      }
+    }
+  })
+  $('#box7').on('click', function () {
+    if ($('#box7').html() === '') {
+      if (lastMove === playerTwo) {
+        (event.target).append(playerOne)
+        playerMoves[6] = playerOne
+        console.log(playerMoves)
+        lastMove = playerOne
+      } else {
+        (event.target).append(playerTwo)
+        playerMoves[6] = playerTwo
+        console.log(playerMoves)
+        lastMove = playerTwo
+      }
+    }
+  })
+  $('#box8').on('click', function () {
+    if ($('#box8').html() === '') {
+      if (lastMove === playerTwo) {
+        (event.target).append(playerOne)
+        playerMoves[7] = playerOne
+        console.log(playerMoves)
+        lastMove = playerOne
+      } else {
+        (event.target).append(playerTwo)
+        playerMoves[7] = playerTwo
+        console.log(playerMoves)
+        lastMove = playerTwo
+      }
+    }
+  })
+  $('#box9').on('click', function () {
+    if ($('#box9').html() === '') {
+      if (lastMove === playerTwo) {
+        (event.target).append(playerOne)
+        playerMoves[8] = playerOne
+        console.log(playerMoves)
+        lastMove = playerOne
+      } else {
+        (event.target).append(playerTwo)
+        playerMoves[8] = playerTwo
+        console.log(playerMoves)
+        lastMove = playerTwo
+      }
+    }
+  })
+})
